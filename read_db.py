c60488dd5404d6e5a757406d6903ba12e33d24e6
Tasnuva Chowdhury <tachowdh@lxplus982.cern.ch>
2024-09-27 14:58:39 +0200

    new packages

---
+++ read_db.py
@@ -0,0 +1,43 @@
+import cx_Oracle
+import pandas as pd
+import json
+import oracledb
+from time import time
+
+
+output_dir = "/app/output"
+
+# Establish a connection to Oracle
+conn = cx_Oracle.connect(user=database_user, password=database_password, dsn=dsn)
+
+query = """
+SELECT * 
+FROM ATLAS_PANDA.taskS_statuslog
+WHERE JEDITASKID IN (
+    SELECT DISTINCT JEDITASKID
+    FROM ATLAS_PANDA.taskS_statuslog
+    WHERE STATUS = 'scouting'
+    AND JEDITASKID = 40880168
+)
+ORDER BY MODIFICATIONTIME
+"""
+
+# Using cursor to execute the query
+try:
+    cursor = conn.cursor()
+    cursor.execute(query)
+
+    # Fetching data into a DataFrame
+    df = pd.read_sql(query, con=conn)
+    
+    # Display DataFrame info and shape
+    print(df.info(), df.shape)
+    
+    # Save the DataFrame to a CSV file
+    df.to_csv(f"{output_dir}/test.csv", index=False)
+    
+finally:
+    # Close the cursor and connection
+    cursor.close()
+    conn.close()
+
